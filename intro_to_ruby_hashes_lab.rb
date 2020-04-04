be8c51d1b3c756e9c891b93afb07b8c344331fc3
DAZUN XING <dazun.xing@gmail.com>
2020-04-04 07:32:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,11 +16,11 @@ end
 
 def monopoly_with_second_tier
   	monopoly = {
-	  :railroads =>{}
-   railroads ={
-    :pieces =>4,
-    :names =>{},
-    :rent_in_dollars =>{}
+	  :railroads =>{
+  
+      :pieces =>4,
+      :names =>{},
+      :rent_in_dollars =>{}
   }
  
   
@@ -29,12 +29,14 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  	monopoly = {
-	  :railroads =>{}
-   railroads ={
-    :pieces =>4,
-    :names =>{},
-    :rent_in_dollars =>{}
+  		monopoly = {
+	  :railroads =>{
+  
+      :pieces =>4,
+      :names =>{},
+      :rent_in_dollars =>{}
+  }
+ 
   rent_in_dollars ={
     :one_piece_owned =>25,
     :two_piece_owned =>50,
