0ef19800c72672a9ec533ee75a1a28e3c8e8b41d
DAZUN XING <dazun.xing@gmail.com>
2020-04-04 07:02:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,9 +16,9 @@ end
 
 def monopoly_with_second_tier
   
-  railroads[:pieces]=4
-  railroads[:names]={}
-  railroads[:rent_in_dollars]={}
+  [:pieces]=4
+  [:names]={}
+  [:rent_in_dollars]={}
   
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
