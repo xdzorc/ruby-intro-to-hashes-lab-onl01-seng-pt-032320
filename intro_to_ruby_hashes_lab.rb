af45004dc7c29eefce44e0bdb4aa7bb0e4b21646
DAZUN XING <dazun.xing@gmail.com>
2020-04-04 06:59:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,27 +1,47 @@
 def new_hash
-
+  hash=Hash.new
 end
 
 def actor
-
+  actor={:name =>"Dwayne The Rock Johnson"}
 end
 
 def monopoly
 	monopoly = {}
+	monopoly[:railroads]={}
 
 end
 
 def monopoly_with_second_tier
+  
+  railroads[:pieces]=4
+  railroads[:names]={}
+  railroads[:rent_in_dollars]={}
+  
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
 
 def monopoly_with_third_tier
+ 
+  rent_in_dollars[:one_piece_owned]=25
+  rent_in_dollars[:two_piece_owned]=50
+  rent_in_dollars[:three_piece_owned]=100
+  rent_in_dollars[:four_piece_owned]=200
+  name[:reading_railroad]={}
+  name[:pennsylvania_railroad]={}
+  name[:b_and_o_railroad]={}
+  name[:shortline]={}
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
 
 def monopoly_with_fourth_tier
+  
+  reading_railroad[:mortgage_value]="$100"
+  pennsylvania_railroad[:mortgage_value]="$200"
+  b_and_o_railroad[:mortgage_value]="$400"
+  shortline[:mortgage_value]="$800"
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 
 end
