f812da00af4d0460c63627300c8627578f33cec8
DAZUN XING <dazun.xing@gmail.com>
2020-04-04 07:04:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,9 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
- 
+  :rent_in_dollars ={
+    
+  }
   rent_in_dollars[:one_piece_owned]=25
   rent_in_dollars[:two_piece_owned]=50
   rent_in_dollars[:three_piece_owned]=100
