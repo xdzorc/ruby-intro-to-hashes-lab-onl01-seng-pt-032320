c7f40f08322df8dd63aeb76ccecb5bf6a9af0fb3
DAZUN XING <dazun.xing@gmail.com>
2020-04-04 07:09:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -47,10 +47,18 @@ end
 
 def monopoly_with_fourth_tier
   
-  reading_railroad[:mortgage_value]="$100"
-  pennsylvania_railroad[:mortgage_value]="$200"
-  b_and_o_railroad[:mortgage_value]="$400"
-  shortline[:mortgage_value]="$800"
+  reading_railroad={
+    :mortgage_value =>"$100"
+  },
+  pennsylvania_railroad={
+    :mortgage_value =>"$200"
+  },
+  b_and_o_railroad={
+    :mortgage_value =>"$400"
+  },
+  shortline={
+    :mortgage_value =>"$800"
+  }
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 
 end
