b97264cdeed6ccec07acea39cb546a63e2b8d7ff
DAZUN XING <dazun.xing@gmail.com>
2020-04-04 07:44:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,7 @@ def monopoly_with_third_tier
         :shortline =>{}
       },
       :rent_in_dollars =>{
-        :one_pieces_owned =>25,
+        :one_piece_owned =>25,
         :two_pieces_owned =>50,
         :three_pieces_owned =>100,
         :four_pieces_owned =>200
